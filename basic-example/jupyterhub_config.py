1f772a1490ab6b3e125f9289403969fdd403715e
Fotis Nikolaidis <nikolaidis.fotis@gmail.com>
2024-05-21 22:19:44 +0300

    configure for jtw
    Signed-off-by: Fotis Nikolaidis <nikolaidis.fotis@gmail.com>

--- basic-example/jupyterhub_config.py
+++ basic-example/jupyterhub_config.py
@@ -47,10 +47,56 @@ c.JupyterHub.cookie_secret_file = "/data/jupyterhub_cookie_secret"
 c.JupyterHub.db_url = "sqlite:////data/jupyterhub.sqlite"
 
 # Authenticate users with Native Authenticator
-c.JupyterHub.authenticator_class = "nativeauthenticator.NativeAuthenticator"
+#c.JupyterHub.authenticator_class = "nativeauthenticator.NativeAuthenticator"
 
 # Allow anyone to sign-up without approval
 c.NativeAuthenticator.open_signup = True
+
+
+# -------------------------------------------
+# Authenticate users with JTW Authenticator
+c.JupyterHub.authenticator_class = "jwtauthenticator.JSONWebTokenLocalAuthenticator"
+
+# Only one of two following fields must be set.  
+# If both, then "secret" will be the signing method used.
+# The secrect key used to generate the given token
+c.JSONWebTokenAuthenticator.secret = '<secret-key>'   
+         
+# -OR-
+# The certificate used to sign the incoming JSONWebToken, must be in PEM Format
+#c.JSONWebTokenAuthenticator.signing_certificate = '/foo/bar/adfs-signature.crt'
+
+# The claim field contianing the username/sAMAccountNAme/userPrincipalName
+c.JSONWebTokenAuthenticator.username_claim_field = 'username'
+
+# This config option should match the aud field of the JSONWebToken, empty string to disable the validation of this field.
+#c.JSONWebTokenAuthenticator.expected_audience = 'https://myApp.domain.local/'
+c.JSONWebTokenAuthenticator.expected_audience = ''
+
+# This will enable local user creation upon authentication, requires JSONWebTokenLocalAuthenticator
+c.JSONWebLocalTokenAuthenticator.create_system_users = True
+
+# Only the one of three follwing sources for JWT token must be set at the time. If you want to disable inspection
+# of some of sources, set corresponding value to '' 
+
+# URL for redirecting to in the case of invalid auth token
+#c.JSONWebTokenAuthenticator.auth_url = 'https://auth.example.com' 
+c.JSONWebTokenAuthenticator.auth_url = 'http://www.quickmeme.com/img/31/315664151c156dd45dcb9421ce259b352c1867e92fbbde4e66693004c8bd95c1.jpg' 
+
+# Name of query param for auth_url to pass return URL
+c.JSONWebTokenAuthenticator.retpath_param = 'retpath' 
+
+# Header name to retrieve JWT token
+c.JSONWebTokenAuthenticator.header_name = 'X-Auth-Token'  
+ 
+# Cookie name to retrieve JWT token                      
+c.JSONWebTokenAuthenticator.cookie_name = 'auth_token'
+
+# Query param to retrieve JWT token                         
+c.JSONWebTokenAuthenticator.param_name = 'auth_token'
+
+
+# -----------------------------
 
 # Allowed admins
 admin = os.environ.get("JUPYTERHUB_ADMIN")
